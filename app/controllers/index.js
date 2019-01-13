67f7a497fd422076d248320fd9c6fb3f245ff10a
Valter Neto <valter.neto32@gmail.com>
2019-01-13 11:54:26 +0000

    Instalação o ExpressSession, configuração da rota

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,21 @@
 module.exports.index = ( application, req, res ) => {
-    res.render( 'index' )
+    res.render( 'index', { errors: {} } )
+}
+
+module.exports.autenticar = ( application, req, res ) => {
+    //res.render( 'Autenticação do index' )
+    
+    const dados = req.body;
+    
+    req.assert('usuario', 'Usuário não pode ser vazio!').notEmpty()
+    req.assert('senha', 'Senha não pode ser vazia!').notEmpty()
+    
+    const errors = req.validationErrors();
+    
+    if ( errors.length > 0 ) {
+        res.render('index', { errors: errors } )
+        return
+    }
+    
+    res.send('tudo ok para autenticar')
 }
