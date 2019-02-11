08a057b0a4917484aec325b287682ddd74cfb0ea
Valter Neto <valter.neto32@gmail.com>
2019-02-11 07:44:28 +0000

    removendo 'var'

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* importar as configurações do servidor */
-var app = require('./config/server');
+const app = require('./config/server');
 
 /* parametrizar a porta de escuta */
 app.listen(process.env.PORT, function(){
